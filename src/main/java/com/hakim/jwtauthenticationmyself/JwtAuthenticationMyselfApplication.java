574ccd505649f1f6170e33c4694f1cc708d1ee7a
sadiul-hakim <sadiulhakim@gmail.com>
2023-05-10 23:50:04 +0600

    setting up project

---
+++ src/main/java/com/hakim/jwtauthenticationmyself/JwtAuthenticationMyselfApplication.java
@@ -0,0 +1,13 @@
+package com.hakim.jwtauthenticationmyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationMyselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationMyselfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hakim/jwtauthenticationmyself/JwtAuthenticationMyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakim.jwtauthenticationmyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationMyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
